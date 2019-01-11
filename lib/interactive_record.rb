7e555e78d84d9a95d667c7e3be7d9e1563483bf8
Oscar Vuong <oscarvuong@gmail.com>
2019-01-11 23:15:09 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -3,7 +3,7 @@ require 'active_support/inflector'
 
 class InteractiveRecord
 
-
+  attr_accessor :id, :name, :grade
   def self.table_name
 
     "#{self.to_s.downcase}s"
@@ -14,4 +14,10 @@ class InteractiveRecord
     array
 
   end
+
+  def initialize(options={})
+   options.each do |property, value|
+     self.send("#{property}=", value)
+   end
+ end
 end
